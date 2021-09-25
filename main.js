efdb034d646f8537946389f2d8fa536139232d38
saanvipandey <74305880+saanvipandey@users.noreply.github.com>
2021-09-25 15:46:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ function preload()
 }
 
 function setup() {
-	canvas =  createCanvas(600, 500);
+	canvas =  createCanvas(400, 400);
 	canvas.center();
 
 	video = createCapture(VIDEO);
@@ -55,7 +55,7 @@ function gotPoses(results)
 }
 
 function draw() {
-	image(video, 0, 0, 600, 500);
+	image(video, 0, 0, 400, 400);
 	
 	song1_status = song1.isPlaying();
 	song2_status = song2.isPlaying();
